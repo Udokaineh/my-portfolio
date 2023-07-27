c23692e2606c51ba71629959069646aed7d918d5
mag_daleneee <udokaineh04@gmail.com>
2023-07-27 15:28:14 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' });
+body = document.querySelector("body")
+sideHeader = Document.getElementById("hideDiv")
+footer = document.getElementById("footer")
+
